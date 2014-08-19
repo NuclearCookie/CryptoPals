b1b21db0c02a060a00565862d008d784aa44cc74
NuclearCookie <pietervantorre@gmail.com>
2014-08-19 22:39:16 +0200

    Tweaked UI

--- set1/Challenge2/Challenge2.cpp
+++ set1/Challenge2/Challenge2.cpp
@@ -26,7 +26,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		std::cout << "Input HexString" << std::endl;
 		std::string inputString;
 		std::cin >> inputString;
-		std::cout << "XOR value HexString" << std::endl;
+		std::cout << std::endl << "XOR value HexString" << std::endl;
 		std::string xorString;
 		std::cin >> xorString;
 		if(inputString.size() == xorString.size())
@@ -38,9 +38,30 @@ int _tmain(int argc, _TCHAR* argv[])
 			m_BitSequence->XOREqualSizeBuffers(inputStream, xorStream, inputString.size());
 			m_BitSequence->HexToAsciiArray(inputStream, inputStream, inputString.size());
 			std::string outputString(inputStream, inputString.size());
+			std::cout << std::endl << "Result after XOR: " << std::endl;
 			std::cout << outputString << std::endl;
-			int i = 0;
-			std::cin >> i;
+			std::cout << std::endl << "Would you like to do another conversion? (Y/N)" << std::endl;
+			bool correctAnswer = false;
+			while(!correctAnswer)
+			{
+				char answer;
+				std::cin >> answer;
+				if(answer == 'N' || answer == 'n')
+				{
+					correctAnswer = true;
+				}
+				else if(answer == 'Y' || answer == 'y')
+				{
+					correctAnswer = true;
+					retry = true;
+				}
+				else if(!std::cin || correctAnswer == false)
+				{
+					std::cin.clear(); // unset failbit
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // skip bad input
+					std::cout << "Unrecognised answer.. try again!" << std::endl;
+				}
+			}
 			delete[] inputStream;
 			delete[] xorStream;
 		}
